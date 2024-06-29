9a2df39105800770177577588fc69378b0da1fc8
Wawanzz_ <arul.safwan73@gmail.com>
2024-06-29 10:31:54 +0700

    fix bug bootstrap js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 const app = createApp(App)
 
